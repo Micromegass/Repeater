273e31949b604064cddcddbe4cf49ff954ffe075
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-10-11 11:36:31 -0500

    added repetition

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+
+constructor() {
+  super();
+  this.state = {
+
+    repeat: ""
+
+  }
+
+
+}
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.again.bind(this)} placeholder="Empieza a escribir algo" />
+        <p className="repeater"> {this.state.repeat}</p>
       </div>
     );
   }
+
+
+
+
+  again(event) {
+
+    this.setState({
+      repeat: event.target.value
+    });
+
+  }
+
 }
 
 export default App;
